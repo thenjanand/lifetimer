6f7feb940f3b8b2b90bc7960272c108e68dd9738
NEERAJ ANAND <thenjanand@gmail.com>
2023-01-16 23:56:20 +0530

    date of birth capture function added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let isDOBOpen = false;
+let dateOfBirth;
+
 const settingCogEl = document.getElementById("settingIcon");
 const settingContentEl = document.getElementById("settingContent");
-const initialTextEl= 
+const initialTextEl = document.getElementById("initialText");
+const afterDOBBtnTextEl = document.getElementById("afterDOBBtnText");
+const dobButtonEl = document.getElementById("dobButton");
+const dobInputEl = document.getElementById("dobInput");
 
 const toggleDateOfBirthSelector = () => {
   if (isDOBOpen) {
@@ -11,4 +16,10 @@ const toggleDateOfBirthSelector = () => {
   }
   isDOBOpen = !isDOBOpen;
 };
+
+const setDOBHandler = () =>{
+  dateOfBirth= dobInputEl.value;
+  console.log("Date of birth",dateOfBirth);
+};
 settingCogEl.addEventListener("click", toggleDateOfBirthSelector);
+dobButtonEl.addEventListener("click", setDOBHandler); 
